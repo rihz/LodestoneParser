5665c08aa3dcef240a8aca782af20d15aa1f873b
rihz <jshields310@gmail.com>
2019-01-10 15:21:18 -0500

    added base LodestoneParser class with constructor and HtmlAgilityPack.

---
+++ LodestoneParser/LodestoneParser/LodestoneParser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using HtmlAgilityPack;
+
+namespace LodestoneParser
+{
+    public class LodestoneParser
+    {
+        const string _url = "https://na.finalfantasyxiv.com/lodestone/character/{0}/";
+
+        HtmlDocument _doc;
+
+        public LodestoneParser(int userId)
+        {
+            var url = string.Format(_url, userId);
+            var web = new HtmlWeb();
+
+            _doc = web.Load(url);
+        }
+    }
+}
