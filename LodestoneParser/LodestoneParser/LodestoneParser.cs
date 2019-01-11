5c84568459e48ef2007714a02e3c2ad85c4d2634
Josh Shields <jshields310@gmail.com>
2019-01-10 19:28:47 -0500

    added console project to test parser functions. Fixed User-Agent issue. Fixed what element the parser should look for.

--- LodestoneParser/LodestoneParser/LodestoneParser.cs
+++ LodestoneParser/LodestoneParser/LodestoneParser.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 using HtmlAgilityPack;
 
 namespace LodestoneParser
@@ -7,29 +8,30 @@ namespace LodestoneParser
     {
         const string _url = "https://na.finalfantasyxiv.com/lodestone/character/{0}/";
 
-        HtmlDocument _doc;
+        HtmlNode _doc;
 
         public LodestoneParser(int userId)
         {
             var url = string.Format(_url, userId);
             var web = new HtmlWeb();
+            web.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36";
 
-            _doc = web.Load(url);
+            _doc = web.Load(url).DocumentNode;
         }
 
         public string GetTitle()
         {
-            return _doc.DocumentNode.SelectSingleNode("//div[@class='frame__chara__title']").InnerText;
+            return _doc.SelectSingleNode("//p[@class='frame__chara__title']").InnerText;
         }
 
         public string GetName()
         {
-            return _doc.DocumentNode.SelectSingleNode("//div[@class='frame__chara__name']").InnerText;
+            return _doc.SelectSingleNode("//p[@class='frame__chara__name']").InnerText;
         }
 
         public string GetServer()
         {
-            return _doc.DocumentNode.SelectSingleNode("//div[@class='frame__chara__world']").InnerText;
+            return _doc.SelectSingleNode("//p[@class='frame__chara__world']").InnerText;
         }
     }
 }
---
+++ LodestoneParser/ParserConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParserConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var parser = new LodestoneParser.LodestoneParser(6738422);
+            Console.WriteLine(parser.GetTitle());
+            Console.WriteLine(parser.GetName());
+            Console.WriteLine(parser.GetServer());
+        }
+    }
+}
